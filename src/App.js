63a856b5fd98c70697ed6742cbf47fe64b6a5a5b
Tetiana Balakleiets <lovely.ledi@gmail.com>
2023-09-15 18:56:02 +0300

    init react and add button

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Button from './components/Button';
+
+function App() {
+  return (
+    <div>
+      <div className="App">Hello World</div>
+    <Button></Button>
+    </div>
+
+  );
+}
+
+export default App;
