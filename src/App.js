99f49d1e7698faec1f45a76f8615c1f961066447
Tetiana Balakleiets <lovely.ledi@gmail.com>
2023-09-25 19:46:59 +0300

    add CourceCard

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 
 import './App.css';
 import Button from './components/Button/Button';
+import CourceCard from './components/Button/CourceCard/CourceCard';
 
 function App() {
+  const cource = {
+    id: 1,
+    img: 'https://assets.ithillel.ua/images/icons/courses-reach/_transform_icon64_2x/front-end-pro.png',
+    level: "Advanced",
+    title: "Front - end Pro",
+    user: {
+      avatar:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/2048px-User-avatar.svg.png",
+      name: "Tetiana Balakleiets"
+    },
+    rating: 8,
+    students: 17,
+    classes: 38,
+    schedule: "twice a week",
+    duration: 5400
+  }
   return (
-    <div>
-      <div className="App">Hello React</div>
-    <Button text="Clik Me"></Button>
+    <div className='list'>
+      <CourceCard cource={cource}/>
     </div>
 
-  );
+    // <div>
+    //   <div className="App">Hello React</div>
+    //   <Button text="Clik Me"></Button>
+    // </div>
+
+  )
 }
 
 export default App;
---
+++ src/components/Button/CourceCard/CourceCard.jsx
@@ -0,0 +1,31 @@
+import './CourceCard.css';
+
+const CourceCard = (props) => {
+    return (
+        <div class ="card">
+            <div class="image__container">
+                <img src={props.cource.img}alt=""/>
+                <div class="level">{props.cource.level}</div>
+            </div>
+            <h3 class="card__title">{props.cource.title}</h3>
+
+            <div class="card__info">
+                <div class="user">
+                    <img src={props.cource.user.avatar} alt={props.cource.user.name}/>
+                    <p>{props.cource.user.name}</p>
+                </div>
+                <div class="rating">
+                    <p>{props.cource.rating} rating</p>
+                </div>
+            </div>
+            <div class="card__info">
+                <div>{props.cource.students} Students</div>
+                <div>{props.cource.classes} Classes</div>
+                <div>{props.cource.schedule}</div>
+                <div>{props.cource.duration} sec</div>
+            </div>
+        </div>
+
+    )
+}
+export default CourceCard
