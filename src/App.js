6a25574f12bee35c945414e2dceb72810b6302db
Tetiana Balakleiets <lovely.ledi@gmail.com>
2023-09-27 15:16:36 +0300

    change CourceCard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
 import './App.css';
-import Button from './components/Button/Button';
 import CourceCard from './components/Button/CourceCard/CourceCard';
 
 function App() {
-  const cource = {
+
+  const cource_1 = {
     id: 1,
     img: 'https://assets.ithillel.ua/images/icons/courses-reach/_transform_icon64_2x/front-end-pro.png',
     level: "Advanced",
@@ -20,17 +20,29 @@ function App() {
     schedule: "twice a week",
     duration: 5400
   }
+  const cource_2 = {
+    id: 1,
+    img: 'https://assets.ithillel.ua/images/icons/courses-reach/_transform_icon64_2x/front-end-pro.png',
+    level: "Advanced",
+    title: "Front - end Pro",
+    user: {
+      avatar:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/2048px-User-avatar.svg.png",
+      name: "Tetiana Balakleiets"
+    },
+    rating: 8,
+    students: 17,
+    classes: 38,
+    finishedClasses: 18,
+    schedule: "twice a week",
+    duration: 5400
+  }
   return (
     <div className='list'>
-      <CourceCard cource={cource}/>
+      <CourceCard cource={cource_1}/>
+      <CourceCard cource={cource_2} isMyCource={true}/>
     </div>
-
-    // <div>
-    //   <div className="App">Hello React</div>
-    //   <Button text="Clik Me"></Button>
-    // </div>
-
-  )
+ );
 }
 
 export default App;
---
+++ src/useState.js
@@ -0,0 +1,9 @@
+const useState = (initialState)=> {
+    let value = initialState;
+
+    const setValue = (newValue)=> {
+        value = newValue;
+    }
+    return [value, setValue]
+}
+const [value, setValue] = useState(0);
