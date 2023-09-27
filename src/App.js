4b3c4af43eecae035761072802dfd13f8f471895
Tetiana Balakleiets <lovely.ledi@gmail.com>
2023-09-27 15:40:30 +0300

    add Count

--- src/App.js
+++ src/App.js
@@ -1,48 +1,27 @@
 
 import './App.css';
-import CourceCard from './components/Button/CourceCard/CourceCard';
-
+import { useState } from 'react';
 function App() {
 
-  const cource_1 = {
-    id: 1,
-    img: 'https://assets.ithillel.ua/images/icons/courses-reach/_transform_icon64_2x/front-end-pro.png',
-    level: "Advanced",
-    title: "Front - end Pro",
-    user: {
-      avatar:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/2048px-User-avatar.svg.png",
-      name: "Tetiana Balakleiets"
-    },
-    rating: 8,
-    students: 17,
-    classes: 38,
-    schedule: "twice a week",
-    duration: 5400
-  }
-  const cource_2 = {
-    id: 1,
-    img: 'https://assets.ithillel.ua/images/icons/courses-reach/_transform_icon64_2x/front-end-pro.png',
-    level: "Advanced",
-    title: "Front - end Pro",
-    user: {
-      avatar:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/2048px-User-avatar.svg.png",
-      name: "Tetiana Balakleiets"
-    },
-    rating: 8,
-    students: 17,
-    classes: 38,
-    finishedClasses: 18,
-    schedule: "twice a week",
-    duration: 5400
-  }
-  return (
-    <div className='list'>
-      <CourceCard cource={cource_1}/>
-      <CourceCard cource={cource_2} isMyCource={true}/>
+  const [value, setValue] = useState(0);
+  const incrementCount = ()=> {
+    setValue (value + 1);
+  };
+  const descrementCount = ()=> {
+    setValue (value - 1);
+  };
+  const resetCount = ()=> {
+    setValue(0);
+  };
+  return(
+    <div>
+      <h1>Count: {value}</h1>
+      <button onClick={incrementCount}>Increment</button>
+      <button onClick={descrementCount}>Descrement</button>
+      <button onClick={resetCount}>Reset</button>
     </div>
- );
+  )
 }
+
 
 export default App;
