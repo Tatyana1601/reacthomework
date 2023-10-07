9ecf3cf87136650cb521b27eb4cde857fbf255d5
Tetiana Balakleiets <lovely.ledi@gmail.com>
2023-10-07 20:57:43 +0300

    add useEffect fetch

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
+import { useState, useEffect} from 'react';
 
-import './App.css';
-import { useState } from 'react';
 function App() {
 
-  const [value, setValue] = useState(0);
-  const incrementCount = ()=> {
-    setValue (value + 1);
-  };
-  const descrementCount = ()=> {
-    setValue (value - 1);
-  };
-  const resetCount = ()=> {
-    setValue(0);
-  };
-  return(
-    <div>
-      <h1>Count: {value}</h1>
-      <button onClick={incrementCount}>Increment</button>
-      <button onClick={descrementCount}>Descrement</button>
-      <button onClick={resetCount}>Reset</button>
-    </div>
-  )
+  const [type, setType] = useState('users');
+  const [data, setData] = useState([]);
+  
+
+  useEffect( () => {
+    const getUsers = async () =>{
+      try{
+        const response = await fetch(`https://jsonplaceholder.typicode.com/${type}`);
+        if (!response.ok){
+          throw new Error ("Falied to fetch users");
+        }
+        const data = await response.json();
+        setData(data);
+      }catch (e){
+        console.log(e.message)
+      }
+    }
+    getUsers();
+    }, [type]);
+return (
+  <div>
+  <h1>Resurce: {type}</h1>
+          <button onClick={()=> setType("users")}>Users</button>
+          <button onClick={()=> setType("albums")}>Albums</button>
+          <button onClick={()=> setType("posts")}>Posts</button>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+  </div>
+)
 }
-
-
 export default App;
