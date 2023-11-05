0ec45c75163adea1b34d0b9aa2453305907678c9
Tetiana Balakleiets <lovely.ledi@gmail.com>
2023-11-05 19:13:40 +0200

    add CounterSlice

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-
-import Counter from "./components/Counter";
-
-
+import { useDispatch, useSelector } from 'react-redux';
+import { increment, decrement } from './redux/counterSlice';
+import './App.css';
 
 function App() {
-  
+const dispatch = useDispatch();
+const counter = useSelector((store)=>store.counter);
+
+
+const handleIncrement = ()=>{
+  dispatch(increment());
+}
+
+const handleDecrement = ()=>{
+  dispatch(decrement());
+}
+
   return (
-    <Counter/>
+    <div className='App'>
+      <h1>Counter value:{counter.value}</h1>
+      <button className='button' onClick={handleIncrement}>Increment</button>
+      <button className='button' onClick={handleDecrement}>Decrement</button>
+
+    </div>
   )
 }
 export default App;
